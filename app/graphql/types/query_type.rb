defa045f1aacefd9aa282aac78a77d037376343f
Anthony Esmeralda <aresmeralda95@gmail.com>
2023-08-30 22:15:01 -0700

    add more query methods

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,8 +8,23 @@ module Types
       argument :id, ID
     end
 
+    field :post_by_name, [PostType], 'Find a post by name' do
+      argument :title, String
+    end
+
+    field :all_post, [PostType], 'Return all posts'
+
     def post(id:)
       Post.find(id)
     end
+
+    def all_post
+      Post.all
+    end
+
+    def post_by_name(title:)
+      Post.where('title LIKE ?', "%#{title}%")
+    end
+
   end
 end
